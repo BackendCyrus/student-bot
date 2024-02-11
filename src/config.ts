a79461ef89482b8aae65ae20e9e813d81111aa96
godofbet <moarefinia@gmail.com>
2024-02-11 11:17:37 +0330

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,25 @@
-export const userName: string = process.env.USER_NAME || ''
-export const password: string = process.env.PASSWORD || ''
+import dotenv from 'dotenv';
+
+interface IConfig {
+    username: string;
+    password: string;
+}
+
+const envFound = dotenv.config();
+
+if (envFound.error) {
+    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
+
+const { PN_USERNAME, PN_PASSWORD } = process.env;
+
+if (!PN_USERNAME || !PN_PASSWORD) {
+    throw new Error("⚠️  Missing PN_USERNAME or PN_PASSWORD in .env file  ⚠️");
+}
+
+const config: IConfig = {
+    username: PN_USERNAME,
+    password: PN_PASSWORD
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,61 @@
 require('dotenv').config();
-import puppeteer from "puppeteer";
-import { userName, password } from "./config";
+import puppeteer from 'puppeteer';
+import config from './config';
 
 async function loginAndJoinMeet(): Promise<void> {
+    // Launch a new headless browser
     const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
 
     try {
+        // Create a new page in the browser
+        const page = await browser.newPage();
+
+        // Navigate to the login page
         await page.goto('https://lms.mpnu.ac.ir/Identity/Account/Login');
 
-        // Log in
-        await page.type('#Input_UserName', userName);
-        await page.type('#password', password);
+        // Enter username and password
+        await page.type('#Input_UserName', config.username);
+        await page.type('#password', config.password);
+
+        // Click the login button
         const loginButton = (await page.$$('.btn'))[2];
         await loginButton.click();
 
-        await page.waitForSelector('#c-subheader')
+        // Wait for the home page to load
+        await page.waitForSelector('#c-subheader');
 
-        // Go to online class
-        await page.goto('https://lms.mpnu.ac.ir/Student/OnlineClass')
+        // Navigate to the online class page
+        await page.goto('https://lms.mpnu.ac.ir/Student/OnlineClass');
 
-        await page.waitForSelector('#c-subheader')
+        // Wait for the online class page to load
+        await page.waitForSelector('#c-subheader');
 
+        // Delay before attempting to click on a class
         setTimeout(async () => {
             try {
+                // Get the list of available classes
                 const meetings = await page.$$('.d-inline');
+                // If there are classes, click the first one
                 if (meetings.length > 0) {
                     const meetingToClick = meetings[0];
                     await meetingToClick.click();
                 }
             } catch (error) {
-                console.log('No classes are currently being taken.')
+                console.log('No classes are currently being taken.');
             }
         }, 5000);
     } catch (error) {
-        console.error("An error occurred:", error);
+        console.error('An error occurred:', error);
     } finally {
+        // Close the browser
         await browser.close();
     }
 }
 
 async function main(): Promise<void> {
+    // Execute the login and join meet function
     await loginAndJoinMeet();
+    // Schedule the login and join meet function to run every 15 minutes
     setInterval(loginAndJoinMeet, 900000);
 }
 
